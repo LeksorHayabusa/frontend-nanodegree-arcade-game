b66432337071584c0fb4e277d3e0b69f0e72bb84
Alex Nikonov <31586394+LeksorHayabusa@users.noreply.github.com>
2018-05-04 07:51:11 +0200

    Update resources.js

--- js/resources.js
+++ js/resources.js
@@ -4,10 +4,11 @@
  * a simple "caching" layer so it will reuse cached images if you attempt
  * to load the same image multiple times.
  */
+ 'use strict';
 (function() {
-    var resourceCache = {};
-    var loading = [];
-    var readyCallbacks = [];
+    let resourceCache = {};
+    let loading = [];
+    let readyCallbacks = [];
 
     /* This is the publicly accessible image loading function. It accepts
      * an array of strings pointing to image files or a string for a single
